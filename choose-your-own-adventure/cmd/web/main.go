00e487c0768b24b054ed01a556e8fc49673f6893
gemosant <rasim.mutlu@outlook.com>
2021-02-14 22:00:45 +0300

    there is a bug that i could not fix

--- choose-your-own-adventure/cmd/web/main.go
+++ choose-your-own-adventure/cmd/web/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	adventure "github.com/project-with-golang/choose-your-own-adventure"
 )
 
 func main() {
+	port := flag.Int("port", 1999, "the port of")
+
 	dosya := flag.String("file", "adventure.json", "the json file") //dosya: adventure.json
 	flag.Parse()
 	fmt.Printf("Using story from %s file\n", *dosya)
@@ -23,5 +27,8 @@ func main() {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	// fmt.Printf("%+v\n", story)
+	h := adventure.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
